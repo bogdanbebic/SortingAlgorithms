5420a1309f681bd9870edaaeb1886cfd3d5e12c7
UrosKrstic <37953231+UrosKrstic@users.noreply.github.com>
2018-12-21 11:29:23 +0100

    Implemented shell sort and created sorting cpp and h files

---
+++ SortingAlgorithms/SortingAlgorithms/sort.cpp
@@ -0,0 +1,52 @@
+#include "sort.h"
+
+/**\file sort.cpp
+ * \brief Definitions of sorting functions
+ */
+
+// TODO: make functions for generating these sequences
+std::vector<int> sorting::Hibbard_gap_sequence {
+	255,
+	127,
+	63,
+	31,
+	15,
+	7,
+	3,
+	1
+};
+std::vector<int> sorting::Pratt_gap_sequence {
+	1093,
+	364,
+	121,
+	40,
+	13,
+	4,
+	1
+};
+std::vector<int> sorting::Ciura_gap_sequence{
+	701,
+	301,
+	132,
+	57,
+	23,
+	10,
+	4,
+	1
+};
+
+
+template<typename T> void sorting::shell_sort(std::vector<T>& v, bool(*cmp)(T lhs, T rhs)) {
+	for (int i = 0; i < Ciura_gap_sequence.capacity(); i++) {
+		int gap = Ciura_gap_sequence[i];
+		for (int j = gap + 1; j < v.capacity(); j++) {
+			T key = v[j];
+			int k = gap - j;
+			while (k >= 0 && (*cmp)(v[j], key)) {
+				v[k + gap] = v[k];
+				k = k - gap;
+			}
+			v[k + gap] = key;
+		}
+	}
+}
---
+++ SortingAlgorithms/SortingAlgorithms/sort.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef _SORT_H_
+#define _SORT_H_
+#include <vector>
+
+namespace  sorting {
+
+	/**
+	 * \brief gap sequences used for shell sort
+	 */
+	extern std::vector<int> Hibbard_gap_sequence;
+	extern std::vector<int> Pratt_gap_sequence;
+	extern std::vector<int> Ciura_gap_sequence;
+
+	/**
+	* \brief Sorts the given vector of numbers using shell sort
+	* \param v Vector that is sorted
+	* \param cmp Function that compares two elements
+	*/
+	template<typename T> void shell_sort(std::vector<T>& v, bool (*cmp) (T lhs, T rhs));
+}
+
+#endif 
