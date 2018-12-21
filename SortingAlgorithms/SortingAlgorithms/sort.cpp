bd2e5a68dd1cd33efa2570896f5b74ecb2c0cd54
Bogdan Bebić <bb170011d@student.etf.bg.ac.rs>
2018-12-21 11:58:08 +0100

    Updated shell sort and documentation
    @uroskrstic

--- SortingAlgorithms/SortingAlgorithms/sort.cpp
+++ SortingAlgorithms/SortingAlgorithms/sort.cpp
@@ -5,7 +5,7 @@
  */
 
 // TODO: make functions for generating these sequences
-std::vector<int> sorting::Hibbard_gap_sequence {
+std::vector<int> sorting::hibbard_gap_sequence {
 	255,
 	127,
 	63,
@@ -15,7 +15,7 @@ std::vector<int> sorting::Hibbard_gap_sequence {
 	3,
 	1
 };
-std::vector<int> sorting::Pratt_gap_sequence {
+std::vector<int> sorting::pratt_gap_sequence {
 	1093,
 	364,
 	121,
@@ -24,7 +24,7 @@ std::vector<int> sorting::Pratt_gap_sequence {
 	4,
 	1
 };
-std::vector<int> sorting::Ciura_gap_sequence{
+std::vector<int> sorting::ciura_gap_sequence{
 	701,
 	301,
 	132,
@@ -36,17 +36,23 @@ std::vector<int> sorting::Ciura_gap_sequence{
 };
 
 
-template<typename T> void sorting::shell_sort(std::vector<T>& v, bool(*cmp)(T lhs, T rhs)) {
-	for (int i = 0; i < Ciura_gap_sequence.capacity(); i++) {
-		int gap = Ciura_gap_sequence[i];
+template<typename T>
+void sorting::shell_sort(std::vector<T>& v, bool(*cmp)(T lhs, T rhs)) {
+	for (int i = 0; i < ciura_gap_sequence.capacity(); i++) {
+		int gap = ciura_gap_sequence[i];
 		for (int j = gap + 1; j < v.capacity(); j++) {
 			T key = v[j];
 			int k = gap - j;
-			while (k >= 0 && (*cmp)(v[j], key)) {
+			while (k >= 0 && (*cmp)(key, v[j])) {
 				v[k + gap] = v[k];
 				k = k - gap;
 			}
 			v[k + gap] = key;
 		}
 	}
+}
+
+template<typename T>
+void sorting::insertion_sort(std::vector<T>& v, bool(*cmp)(T lhs, T rhs)) {
+
 }
--- SortingAlgorithms/SortingAlgorithms/sort.h
+++ SortingAlgorithms/SortingAlgorithms/sort.h
@@ -8,16 +8,19 @@ namespace  sorting {
 	/**
 	 * \brief gap sequences used for shell sort
 	 */
-	extern std::vector<int> Hibbard_gap_sequence;
-	extern std::vector<int> Pratt_gap_sequence;
-	extern std::vector<int> Ciura_gap_sequence;
+	extern std::vector<int> hibbard_gap_sequence;
+	extern std::vector<int> pratt_gap_sequence;
+	extern std::vector<int> ciura_gap_sequence;
 
 	/**
 	* \brief Sorts the given vector of numbers using shell sort
 	* \param v Vector that is sorted
-	* \param cmp Function that compares two elements
+	* \param cmp Function that compares two elements (cmp should return lhs < rhs for ascending sort)
 	*/
 	template<typename T> void shell_sort(std::vector<T>& v, bool (*cmp) (T lhs, T rhs));
+
+	template<typename T> void insertion_sort(std::vector<T>& v, bool(*cmp) (T lhs, T rhs));
+
 }
 
 #endif 
